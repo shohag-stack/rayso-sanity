791adae62f0ac868160136644178eb087d7c0496
shohag-stack <shohag4y@gmail.com>
2026-04-11 13:42:14 +0600

    env updated for sanity

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  token: process.env.SANITY_DATASET_TOKEN,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  token: process.env.NEXT_PUBLIC_SANITY_DATASET_TOKEN,
   apiVersion: "2024-01-01",
   useCdn: false,
 });
